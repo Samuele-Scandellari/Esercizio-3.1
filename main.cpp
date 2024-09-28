5c208eae31844dcf071b80310395843c65a118fe
Samuele-Scandellari <samuscan01@gmail.com>
2024-09-28 15:55:12 +0200

    Esercizio 3.1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int numero;
+    scanf("%d", &numero);
+    if(numero %2 == 0);{
+    printf("%d è un numero pari\n", numero);
+   }else{
+    printf("%d è un numero dispari\n", numero);
+   }
+}
